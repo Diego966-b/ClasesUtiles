afb5f4d4a7cf62199a86c77facac454e5921a3b3
Diego966-b <diegobennjaminn@gmail.com>
2023-10-02 20:49:51 -0300

    carpeta clases

--- utils/holaMundoPdf.php
+++ utils/holaMundoPdf.php
@@ -12,12 +12,16 @@
     <?php
         include_once "domPdf/vendor/autoload.php";
         use Dompdf\Dompdf;
+
         $dompdf = new Dompdf();
+        $options = $dompdf->getOptions();
+        $options->setDefaultFont('Courier');
+        $dompdf->setOptions($options);
         $dompdf->loadHtml('<h1>Hola mundo</h1><br><a href="https://parzibyte.me/blog">By Parzibyte</a>');
         $dompdf->render();
         header("Content-type: application/pdf");
         header("Content-Disposition: inline; filename=documento.pdf");
-        echo $dompdf->output();
+        $dompdf->stream();
     ?>
 </body>
 </html>
