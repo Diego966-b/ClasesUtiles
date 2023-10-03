4c3ec6296314137dc963e37f692ec5a0901615ca
Marco Alejandro Galan Reise <Marco.Galan@energianeuquen.local>
2023-10-03 11:53:13 -0300

    Se creo el archivo mostrarCv.php

---
+++ vista/mostrarCv.php
@@ -0,0 +1,55 @@
+<?php
+include_once("../config.php");
+$pagSeleccionada = "Mostrar CV";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<a href="https://www.flaticon.es/iconos-animados-gratis/descargar" title="descargar iconos animados">Descargar iconos animados creados por Freepik - Flaticon</a>
+<a href="https://www.flaticon.es/iconos-animados-gratis/correo-electronico" title="correo electrónico iconos animados">Correo electrónico iconos animados creados por Freepik - Flaticon</a>
+
+<head>
+    <?php include_once($ESTRUCTURA . "/header.php"); ?>
+</head>
+
+<body>
+    <?php include_once($ESTRUCTURA . "/cabecera.php"); ?>
+    <div class="container-fluid">
+
+        <div class="col-6 p-2 mb-2">
+            <div class="container-cv container-lg">
+                <p>Aca va la vista previa del cv</p>
+                <p>Aca va la vista previa del cv</p>
+                <p>Aca va la vista previa del cv</p>
+            </div>
+        </div>
+
+        <div class="col-sm-3 p-2">
+         <section>
+                <button class="btn btn-success">Descargar
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-in-down" viewBox="0 0 16 16">
+                        <path fill-rule="evenodd" d="M3.5 6a.5.5 0 0 0-.5.5v8a.5.5 0 0 0 .5.5h9a.5.5 0 0 0 .5-.5v-8a.5.5 0 0 0-.5-.5h-2a.5.5 0 0 1 0-1h2A1.5 1.5 0 0 1 14 6.5v8a1.5 1.5 0 0 1-1.5 1.5h-9A1.5 1.5 0 0 1 2 14.5v-8A1.5 1.5 0 0 1 3.5 5h2a.5.5 0 0 1 0 1h-2z" />
+                        <path fill-rule="evenodd" d="M7.646 11.854a.5.5 0 0 0 .708 0l3-3a.5.5 0 0 0-.708-.708L8.5 10.293V1.5a.5.5 0 0 0-1 0v8.793L5.354 8.146a.5.5 0 1 0-.708.708l3 3z" />
+                    </svg>
+                </button>
+
+                <button class="btn btn-primary">
+                    Enviar Correo <img src="imagenes/mensaje.gif" alt="" style="width: 25px; height: 25px;">
+                </button>
+                </section>
+        </div>
+
+    </div>
+
+
+
+    <?php include_once($ESTRUCTURA . "/pie.php"); ?>
+    <script src="<?php echo $JS ?>/validar.js"></script>
+    <style>
+        .container-cv {
+            background-color: gray;
+        }
+    </style>
+</body>
+
+</html>
